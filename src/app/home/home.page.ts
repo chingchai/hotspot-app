5343b84e0aa8b67ecf81318831e46cbf1d6b99ab
aegistz <teerayoot5056@gmail.com>
2019-07-22 21:12:07 +0700

    update scss bg & report

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: Map;
+
+
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
+
+
+  leafletMap() {
+    this.map = new Map('map').setView([16.738560, 100.207789], 6);
+
+    tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }).addTo(this.map);
+
+    const markPoint = marker([12.972442, 77.594563]);
+    markPoint.bindPopup('<p>Tashi Delek - Bangalore.</p>');
+    this.map.addLayer(markPoint);
+  }
 
 }
